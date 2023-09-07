605851752a22841653d3a5bb847186b9b9a73224
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-09-07 10:21:46 +0630

    project setup with tailwindcss

---
+++ react-jwt-auth/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-semibold mt-10">Hello from React Spring JWT Authentication and Authorization</h1>
+    </div>
+  )
+}
+
+export default App
